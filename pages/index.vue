b88b6f1e104494706b83d53493ba074a8bc0682a
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2020-12-28 15:19:41 +0900

    取得したデータのうち、ねこのデータの配列を cats に代入するように調整

--- pages/index.vue
+++ pages/index.vue
@@ -4,15 +4,20 @@
 
 <script>
 export default {
+  data() {
+    return {
+      cats: [],
+    }
+  },
   async created() {
     try {
       const response = await this.$axios.get(
         'https://jsondata.okiba.me/v1/json/7lV2J201227155106'
       )
-      console.log(response)
+      this.cats = response.data.results
     } catch (err) {
       const res = err.response
-      console.log(res)
+      alert(res)
     }
   },
 }
