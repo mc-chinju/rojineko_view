1a79107f82402f4a127047ee9a98abcdcac53e7e
mc-chinju <takeiteasy.enjoyeverything@gmail.com>
2020-12-28 15:07:17 +0900

    トップページで、ねこの情報を取得する API を呼び出す

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,17 @@
 </template>
 
 <script>
-export default {}
+export default {
+  async created() {
+    try {
+      const response = await this.$axios.get(
+        'https://jsondata.okiba.me/v1/json/7lV2J201227155106'
+      )
+      console.log(response)
+    } catch (err) {
+      const res = err.response
+      console.log(res)
+    }
+  },
+}
 </script>
